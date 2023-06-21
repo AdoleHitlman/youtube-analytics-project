92e50475f91709d269ff2920864e1239a945e335
root <root@DESKTOP-KGC7C0L>
2023-06-21 18:56:00 +0300

    !

--- src/playlist.py
+++ src/playlist.py
@@ -7,9 +7,7 @@ from googleapiclient.discovery import build
 
 
 #import json
-
-
-class PlayList:
+class PlayListMixin:
     DEVELOPER_KEY = os.getenv('YT_API_KEY')
     YOUTUBE_API_SERVICE_NAME = "youtube"
     YOUTUBE_API_VERSION = "v3"
@@ -18,19 +16,27 @@ class PlayList:
 
     def __init__(self, playlist_id):
         self.playlist_id = playlist_id
-        self.playlist_dir = PlayList.youtube.playlistItems().list(playlistId=self.playlist_id, part='contentDetails',maxResults=50, ).execute()
+        self.playlist_dir = PlayListMixin.youtube.playlistItems().list(playlistId=self.playlist_id, part='contentDetails',
+                                                                  maxResults=50, ).execute()
         self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_dir['items']]
-        self.channel_id = PlayList.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=self.video_ids[0]).execute()['items'][0]['snippet']["channelId"]
-        self.playlist_info = PlayList.youtube.playlists().list(channelId=self.channel_id,part='contentDetails,snippet',maxResults=50,).execute()
+        self.channel_id = PlayListMixin.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                         id=self.video_ids[0]).execute()['items'][0]['snippet'][
+            "channelId"]
+        self.playlist_info = PlayListMixin.youtube.playlists().list(channelId=self.channel_id, part='contentDetails,snippet',
+                                                               maxResults=50, ).execute()
         self.title = self.playlist_info["items"][0]["snippet"]["title"]
         self.url = f"https://www.youtube.com/playlist?list={playlist_id}"
         self.sec = 0
-        for video in PlayList.youtube.videos().list(part='contentDetails,statistics',id=','.join(self.video_ids)).execute()['items']:
+        for video in \
+        PlayListMixin.youtube.videos().list(part='contentDetails,statistics', id=','.join(self.video_ids)).execute()[
+            'items']:
             # YouTube video duration is in ISO 8601 format
             iso_8601_duration = video['contentDetails']['duration']
             duration = isodate.parse_duration(iso_8601_duration)
             self.sec += duration.seconds
         self.total_duration = datetime.timedelta(seconds=self.sec)
+
+class PlayList(PlayListMixin):
     def show_best_video(self):
         max_likes = 0
         best_video = None

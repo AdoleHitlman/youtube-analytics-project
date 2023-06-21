ccd0580baa4f6a5548620068cc256504e8100d3a
root <root@DESKTOP-KGC7C0L>
2023-06-21 20:06:32 +0300

    last

--- src/video.py
+++ src/video.py
@@ -1,10 +1,24 @@
+import requests
+import os
+
+
 class Video:
     def __init__(self, video_id):
         self.id = video_id
-        self.title = 'GIL в Python: зачем он нужен и как с этим жить'
-        self.video_link = 'https://www.youtube.com/watch?v=AWX4JnAnjBE'
-        self.views_count = 1000
-        self.likes_count = 20
+        DEVELOPER_KEY = os.getenv('YT_API_KEY')
+        try:
+            url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id={video_id}&key={DEVELOPER_KEY}"
+            response = requests.get(url)
+            self.data = response.json()
+            self.title = self.data['items'][0]['snippet']['title']
+            self.views_count = self.data['items'][0]['statistics']['viewCount']
+            self.like_count = self.data['items'][0]['statistics']['likeCount']
+            self.video_link = f"https://www.youtube.com/watch?v={video_id}"
+        except (KeyError, requests.exceptions.RequestException,IndexError):
+            self.title = None
+            self.views_count = None
+            self.like_count = None
+            self.video_link = f"https://www.youtube.com/watch?v={video_id}"
 
     def __str__(self):
         return self.title
@@ -16,4 +30,4 @@ class PLVideo(Video):
         self.playlist_id = playlist_id
 
     def __str__(self):
-        return 'MoscowPython Meetup 78 - вступление'
+        return self.title
--- tests/test_video.py
+++ tests/test_video.py
@@ -9,3 +9,8 @@ def test_video():
 def test_pl_video():
     video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
     assert str(video2) == 'MoscowPython Meetup 78 - вступление'
+
+def test_broken_video_id():
+    broken_video = Video('broken_video_id')
+    assert broken_video.title is None
+    assert broken_video.like_count is None
